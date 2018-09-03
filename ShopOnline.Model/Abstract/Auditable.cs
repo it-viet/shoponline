e11b9ba22767ca45984d6f596e63b0f85bf60c76
it-viet <ltv.nnthanh@gmail.com>
2018-09-03 20:01:35 +0700

    add domain antities for project

---
+++ ShopOnline.Model/Abstract/Auditable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }
+
+        [MaxLength(256)] //Độ dài trong database
+        public string CreatedBy { set; get; }
+        public DateTime? UpdatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdatedBy { set; get; }
+
+        //SEO
+        [MaxLength(256)]
+        public string MetaKeyword { set; get; }
+        [MaxLength(256)]
+        public string MetaDescription { set; get; }
+
+        //Switch
+        public bool Status { set; get; }
+    }
+}
---
+++ ShopOnline.Model/Abstract/IAuditable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+
+        //SEO
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+
+        //Switch
+        bool Status { set; get; }
+    }
+}
